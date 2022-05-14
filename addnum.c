9a95dadf8c924936eb97ab4b5e9a06ad8d8e03db
Forster112 <forstermichael112@gmail.com>
2022-05-14 14:48:59 -0700

    task 0

---
+++ addnum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * addnum - adds two numbers
+ * @first: first number
+ * @second: second number
+ * Return: the total
+ */
+int addnum(int first, int second)
+{
+	int total;
+
+	total = first + second;
+
+	return (total);
+}
