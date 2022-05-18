ceaa3072ea77be75ed63a7800d4ffc4005ac763c
Forster112 <forstermichael112@gmail.com>
2022-05-18 15:22:24 -0700

    main shell

---
+++ shell.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+
+/**
+ * main - entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	unsigned int start;
+
+	do {
+		start = _shell_instance();
+		if (start == 3)
+			start = 0;
+	} while (stat);
+	return (0);
+}
