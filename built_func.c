8af73a5d8f06ca61feed16362a97e3cb987a59ff
Forster112 <forstermichael112@gmail.com>
2022-05-19 01:06:31 +0100

    betty

--- built_func.c
+++ built_func.c
@@ -37,31 +37,3 @@ int _bin_env(__attribute__((unused)) char **args)
 	}
 	return (2);
 }
-/**
- * _exit_bin - exit shell
- * @args: argument array
- * Return: _SHELL_END_ code on success
- */
-int _exit_bin(__attribute__((unused)) char **args)
-{
-	return (3);
-}
-/**
- * _bin_cd - change directory
- * @args: argument array
- * Return: _SKIP_ code on success
- */
-int _bin_cd(char **args)
-{
-	int stat;
-
-	if (args[1] == NULL)
-		stat = chdir(_find_env_get_value("HOME"));
-	else if (_strcmp(args[1], "-") == 0)
-		stat = chdir(_find_env_get_value("OLDPWD"));
-	else
-		stat = chdir(args[1]);
-	if (stat == -1)
-		perror("cd had an error");
-	return (2);
-}
