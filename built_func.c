41aa39e6fcb3aa7f7626262029eae3de12dfd1fa
Forster112 <forstermichael112@gmail.com>
2022-05-18 17:00:28 -0700

    betty

--- built_func.c
+++ built_func.c
@@ -6,20 +6,19 @@
  */
 int run_built_in(char **args)
 {
-    int i = 0;
-    b_ins binsarr[] = {
-        {"env", _bin_env},
-        {"exit", _exit_bin},
-        {"cd", _bin_cd},
-        {NULL, NULL}};
+	int i = 0;
 
-    while (binsarr[i].command != NULL)
-    {
-        if (_strcmp(args[0], binsarr[i].command) == 0)
-            return (binsarr[i].func(args));
-        i++;
-    }
-    return (1);
+	b_ins binsarr[] = {
+		{"env", _bin_env},
+		{"exit", _exit_bin},
+		{"cd", _bin_cd},
+		{NULL, NULL}};
+	while (binsarr[i].command != NULL){
+		if (_strcmp(args[0], binsarr[i].command) == 0)
+			return (binsarr[i].func(args));
+		i++;
+	}
+	return (1);
 }
 /**
  * _bin_env - print environment
@@ -28,16 +27,15 @@ int run_built_in(char **args)
  */
 int _bin_env(__attribute__((unused)) char **args)
 {
-    int i = 0, len;
-
-    while (environ[i] != NULL)
-    {
-        len = _strlen(environ[i]);
-        write(STDOUT_FILENO, environ[i], len);
-        write(STDOUT_FILENO, "\n", 1);
-        i++;
-    }
-    return (2);
+	int i = 0, len;
+	
+	while (environ[i] != NULL){
+		len = _strlen(environ[i]);
+		write(STDOUT_FILENO, environ[i], len);
+		write(STDOUT_FILENO, "\n", 1);
+		i++;
+	}
+	return (2);
 }
 /**
  * _exit_bin - exit shell
@@ -46,7 +44,7 @@ int _bin_env(__attribute__((unused)) char **args)
  */
 int _exit_bin(__attribute__((unused)) char **args)
 {
-    return (3);
+	return (3);
 }
 /**
  * _bin_cd - change directory
@@ -55,21 +53,15 @@ int _exit_bin(__attribute__((unused)) char **args)
  */
 int _bin_cd(char **args)
 {
-    /* char buff[_SHIA_LA_BEAUF_]; */
-    /* char buff2[_SHIA_LA_BEAUF_]; */
-    int stat;
+	int stat;
 
-    /* printf("[%d]\n", _set_env("OLDPWD", getcwd(buff, _SHIA_LA_BEAUF_))); */
-
-    if (args[1] == NULL)
-        stat = chdir(_find_env_get_value("HOME"));
-    else if (_strcmp(args[1], "-") == 0)
-        stat = chdir(_find_env_get_value("OLDPWD"));
-    else
-        stat = chdir(args[1]);
-
-    /* _set_env("PWD", getcwd(buff2, _SHIA_LA_BEAUF_)); */
-    if (stat == -1)
-        perror("cd had an error");
-    return (2);
+	if (args[1] == NULL)
+		stat = chdir(_find_env_get_value("HOME"));
+	else if (_strcmp(args[1], "-") == 0)
+		stat = chdir(_find_env_get_value("OLDPWD"));
+	else
+		stat = chdir(args[1]);
+	if (stat == -1)
+		perror("cd had an error");
+	return (2);
 }
